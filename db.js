20ac9b63ac1bdbee40886b8d1b83a4c97bb569a6
BboyStatix <statix231@hotmail.com>
2020-02-12 07:40:48 +0800

    Create basic database of Apps

---
+++ db.js
@@ -0,0 +1,68 @@
+const Apps = [
+    {
+        id: '1',
+        icon: 'https://p1.hiclipart.com/preview/379/165/122/deus-ex-mankind-divided-dock-icon-png-clipart-thumbnail.jpg',
+        name: 'Deus Ex',
+        headline: 'I never asked for this',
+        description: 'Adam Jensen is back!!! Uncover the truth about Sarif Industries',
+        installed: true
+    },
+    {
+        id: '2',
+        icon: 'https://p1.hiclipart.com/preview/379/165/122/deus-ex-mankind-divided-dock-icon-png-clipart-thumbnail.jpg',
+        name: 'Deus Ex',
+        headline: 'I never asked for this',
+        description: '',
+        installed: false
+    },
+    {
+        id: '3',
+        icon: 'https://p1.hiclipart.com/preview/379/165/122/deus-ex-mankind-divided-dock-icon-png-clipart-thumbnail.jpg',
+        name: 'Deus Ex',
+        headline: 'I never asked for this',
+        description: '',
+        installed: true
+    },
+    {
+        id: '4',
+        icon: 'https://p1.hiclipart.com/preview/379/165/122/deus-ex-mankind-divided-dock-icon-png-clipart-thumbnail.jpg',
+        name: 'Deus Ex',
+        headline: 'I never asked for this',
+        description: '',
+        installed: false
+    },
+    {
+        id: '5',
+        icon: 'https://p1.hiclipart.com/preview/379/165/122/deus-ex-mankind-divided-dock-icon-png-clipart-thumbnail.jpg',
+        name: 'Deus Ex',
+        headline: 'I never asked for this',
+        description: '',
+        installed: false
+    },
+    {
+        id: '6',
+        icon: 'https://p1.hiclipart.com/preview/379/165/122/deus-ex-mankind-divided-dock-icon-png-clipart-thumbnail.jpg',
+        name: 'Deus Ex',
+        headline: 'I never asked for this',
+        description: '',
+        installed: true
+    },
+    {
+        id: '7',
+        icon: 'https://p1.hiclipart.com/preview/379/165/122/deus-ex-mankind-divided-dock-icon-png-clipart-thumbnail.jpg',
+        name: 'Deus Ex',
+        headline: 'I never asked for this',
+        description: '',
+        installed: true
+    },
+    {
+        id: '8',
+        icon: 'https://p1.hiclipart.com/preview/379/165/122/deus-ex-mankind-divided-dock-icon-png-clipart-thumbnail.jpg',
+        name: 'Deus Ex',
+        headline: 'I never asked for this',
+        description: '',
+        installed: false
+    }
+]
+
+module.exports = Apps
