4e8db0267cfcc038b26fb3a675355cc94a7a0b5c
BboyStatix <statix231@hotmail.com>
2020-02-12 09:11:17 +0800

    Update db

--- db.js
+++ db.js
@@ -9,18 +9,18 @@ const Apps = [
     },
     {
         id: '2',
-        icon: 'https://p1.hiclipart.com/preview/379/165/122/deus-ex-mankind-divided-dock-icon-png-clipart-thumbnail.jpg',
-        name: 'Deus Ex',
-        headline: 'I never asked for this',
-        description: '',
+        icon: 'https://i1.pngguru.com/preview/453/604/23/the-witcher-3-wild-hunt-icon-tw3-png-clipart.jpg',
+        name: 'Witcher 3',
+        headline: 'He still hates portals!',
+        description: 'Join Geralt of Rivia on his latest adventure',
         installed: false
     },
     {
         id: '3',
-        icon: 'https://p1.hiclipart.com/preview/379/165/122/deus-ex-mankind-divided-dock-icon-png-clipart-thumbnail.jpg',
-        name: 'Deus Ex',
-        headline: 'I never asked for this',
-        description: '',
+        icon: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8f8830f0-679a-4974-977c-bd5802617c82/db0xyzd-ee42f1c5-5004-4114-bc21-c527b2794c28.png/v1/fill/w_300,h_300,strp/horizon_zero_dawn_icon_by_troublem4ker_db0xyzd-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MzAwIiwicGF0aCI6IlwvZlwvOGY4ODMwZjAtNjc5YS00OTc0LTk3N2MtYmQ1ODAyNjE3YzgyXC9kYjB4eXpkLWVlNDJmMWM1LTUwMDQtNDExNC1iYzIxLWM1MjdiMjc5NGMyOC5wbmciLCJ3aWR0aCI6Ijw9MzAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.ZHU0xVdWL4LIDV8xo1SGBTVvl7qhdzk0DQVL6EDy6xk',
+        name: 'Horizon Zero Dawn',
+        headline: 'Robot Dinosaurs!',
+        description: 'Yep you heard that right',
         installed: true
     },
     {
